9207099596beab637c6dde2d8f2a2534f5b4a3b9
hs0071 <bootstrap-cyborg.min>
2021-02-15 14:01:35 +1300

    Add project files.

---
+++ StudentMgnt_WebAPI/Data/StudentMgnt_WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentMgnt_WebAPI.Models;
+
+namespace StudentMgnt_WebAPI.Data
+{
+    public class StudentMgnt_WebAPIContext : DbContext
+    {
+        public StudentMgnt_WebAPIContext (DbContextOptions<StudentMgnt_WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentMgnt_WebAPI.Models.Student> Student { get; set; }
+    }
+}
---
+++ StudentMgnt_WebAPI/Migrations/20210214215451_Init.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentMgnt_WebAPI.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    student_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    student_Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    student_Mobile = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    course_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    course_Duration = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
